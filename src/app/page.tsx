9e0b9851a04255953b3d63ff2a11426f02d894ff
Miguel Munoz E <miguelestradam12@gmail.com>
2024-11-13 12:14:45 -0500

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
 import Link from "next/link";
 
+const mockUrls:Array<string>= [
+  
+]
+const mockImages = mockUrls.map((url: string,index: number)=> ({
+  id: index+1,
+  url
+}))
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="flex flex-wrap gap-4" >
+        {[...mockImages].map((image)=>(
+          <div className="w-48">
+            <img src={image.url} alt="" />
+          </div>
+        ))}
+      </div>
       Hello (gallery in process)
     </main>
   );
